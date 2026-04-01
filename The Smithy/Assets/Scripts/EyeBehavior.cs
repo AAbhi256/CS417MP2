261a7e2e27bab9531bf2f5a63b2d04c6afe09d68
AAbhi256 <abhinava2560@gmail.com>
2026-04-01 00:32:06 -0500

    portals immersive

--- The Smithy/Assets/Scripts/EyeBehavior.cs
+++ The Smithy/Assets/Scripts/EyeBehavior.cs
@@ -9,7 +9,7 @@ public class EyeBehavior : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        this.ShowSurprise();
+        
     }
 
     // Update is called once per frame
