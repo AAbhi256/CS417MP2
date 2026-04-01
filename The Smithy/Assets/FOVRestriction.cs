a7c4906d9feb8f0bf389dba135a2600c89283ff7
Byu33 <byu33@illinois.edu>
2026-03-31 19:39:51 -0500

    bug fix to FOV restriction

--- The Smithy/Assets/FOVRestriction.cs
+++ The Smithy/Assets/FOVRestriction.cs
@@ -1,36 +1,39 @@
 using UnityEngine;
-using Unity.XR.CoreUtils;
 
 [RequireComponent(typeof(Camera))]
 public class FOVRestriction : MonoBehaviour
 {
     [Header("FOV Settings")]
-    public float movingFOV = 80f;
+    public float movingFOV = 40f;
     public float fovTransitionSpeed = 5f;
 
     private Camera cam;
-    private XROrigin xrOrigin;
     private Vector3 lastPosition;
     private float defaultFOV;
 
     void Start()
     {
         cam = GetComponent<Camera>();
-        defaultFOV = cam.fieldOfView; // captures whatever XR sets by default
-        xrOrigin = FindObjectOfType<XROrigin>();
-        lastPosition = xrOrigin.transform.position;
+        defaultFOV = cam.fieldOfView;
+        lastPosition = cam.transform.position;
     }
 
     void Update()
     {
-        Vector3 currentPosition = xrOrigin.transform.position;
+        Vector3 currentPosition = cam.transform.position;
         Vector3 delta = currentPosition - lastPosition;
         delta.y = 0f;
 
         bool isMoving = delta.magnitude / Time.deltaTime > 0.1f;
         lastPosition = currentPosition;
 
-        float targetFOV = isMoving ? movingFOV : defaultFOV;
-        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, Time.deltaTime * fovTransitionSpeed);
+        if (isMoving)
+        {
+            cam.fieldOfView = movingFOV; // snap instantly to narrow FOV
+        }
+        else
+        {
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, defaultFOV, Time.deltaTime * fovTransitionSpeed); // lerp back smoothly
+        }
     }
 }
