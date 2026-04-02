13484f96b47732224a6018df28fdc8f2cdea853c
AAbhi256 <abhinava2560@gmail.com>
2026-04-01 21:15:03 -0500

    multiplayer almost works, just need to do stuff about room joining and make sure two players join the same room if we make it auto instead of manual. if manual, how to manually do it?

--- The Smithy/Assets/Scripts/PlayerBehavior.cs
+++ The Smithy/Assets/Scripts/PlayerBehavior.cs
@@ -32,6 +32,9 @@ public class PlayerBehavior : MonoBehaviour
     private int playerSelfLayer;
     [SerializeField]
     private TrackedPoseDriver leftHandDriver, rightHandDriver;
+    private bool initialized = false;
+    [SerializeField]
+    private Camera fallbackCamera;
 
     void Awake()
     {
@@ -39,64 +42,71 @@ public class PlayerBehavior : MonoBehaviour
     }
     void Start()
     {
-
-        if (avatar.IsMe)
+        if (avatar != null && avatar.IsMe)
         {
-            head.gameObject.layer = playerSelfLayer;
-            foreach (Transform child in head)
-            {
-                child.gameObject.layer = playerSelfLayer;
-            }
-        }
-        else
-        {
-            leftHandDriver.enabled = false;
-            rightHandDriver.enabled = false;
-            camera.gameObject.SetActive(false);
+            goldAmount = 1000000;
+            gemAmount = 1000000;
+            soulAmount = 0;
+            sellPrice = 1;
+            goldGenerationRateMult = 1;
+            goldGenerationRate = 0;
+            gemGenerationRate = 0;
         }
 
-
-        goldAmount = 1000000;
-        gemAmount = 1000000;
-        soulAmount = 0;
-        sellPrice = 1;
-        goldGenerationRateMult = 1;
-        goldGenerationRate = 0;
-        gemGenerationRate = 0;
-
-        statsAction.action.Enable();
-        statsAction.action.performed += (ctx) => 
+        if (statsAction != null)
         {
-            if (temp == null)
+            statsAction.action.Enable();
+            statsAction.action.performed += (ctx) =>
             {
-                ViewStats();
-            }
-            else
-            {
-                Destroy(temp);
-                temp = null;
-            }
-            
-        };
+                if (temp == null)
+                {
+                    ViewStats();
+                }
+                else
+                {
+                    Destroy(temp);
+                    temp = null;
+                }
+            };
+        }
     }
 
 
     void Update()
     {
+        if (avatar == null) return;
+
+        if (!initialized && avatar.IsMe)
+        {
+            initialized = true;
+            head.gameObject.layer = playerSelfLayer;
+            foreach (Transform child in head)
+            {
+                child.gameObject.layer = playerSelfLayer;
+            }
+            fallbackCamera.gameObject.SetActive(false);
+
+        }
+        else if (!initialized && !avatar.IsMe && avatar.IsPossessed)
+        {
+            initialized = true;
+            if (leftHandDriver != null) leftHandDriver.enabled = false;
+            if (rightHandDriver != null) rightHandDriver.enabled = false;
+            if (camera != null) camera.gameObject.SetActive(false);
+        }
+
         if (avatar.IsMe)
         {
             head.localPosition = camera.transform.localPosition;
             head.rotation = camera.transform.rotation;
+
+            EarnGold();
+            EarnGem();
         }
-
-
-        EarnGold();
-        EarnGem();
     }
 
     public void EarnGold()
     {
-
         goldAmount += 0.5F * prestigeMult * goldGenerationRate * goldGenerationRateMult * Time.deltaTime;
     }
 
@@ -107,6 +117,7 @@ public class PlayerBehavior : MonoBehaviour
 
     public void ViewStats()
     {
+        if (StatsCanvas == null) return;
         temp = Instantiate(StatsCanvas, this.transform.position + new Vector3(1,2,0), this.transform.rotation, null);
         temp.transform.Rotate(Vector3.up * 180);
     }
